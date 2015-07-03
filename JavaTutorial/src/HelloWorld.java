d0febc76dceae861e189db7d5b4be5e28e8e7177
npatrick96 <patrickniyongabo@gmail.com>
2015-07-03 14:36:47 +0200

    First Commit

---
+++ JavaTutorial/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	void printInt(int n){
+		for (int i = 0; i <= n; i++){
+			System.out.print(i + " ");
+		}
+	}
+}
---
+++ JavaTutorial/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("this is the dream we have been dreaming of!!!");
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.printInt(100);
+
+	}
+
+}
